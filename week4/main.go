3d27fd46a7aa891a3a72318243df302e2c64196d
aknur <95418973+aknurkappar@users.noreply.github.com>
2024-09-29 22:21:33 +0500

    Exercise 2: Working with PostgreSQL using GORM

--- week4/main.go
+++ week4/main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
 )
 
 type User struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
-	Age  int64  `json:"age"`
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"size:255"`
+	Age  int64  `json:"not null"`
 }
 
 func connectToDatabase() {
-	connStr := "user=postgres password=secret dbname=postgres port=5433 sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	defer db.Close()
+	connStr := "host=localhost user=postgres password=secret dbname=postgres port=5433 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 
 	if err != nil {
 		fmt.Println("Error in connecting to database:", err)
 	}
-	if err = db.Ping(); err != nil {
-		fmt.Println("Error in pinging database:", err)
+	fmt.Println("Connected to database!")
+
+	db.AutoMigrate(&User{})
+	fmt.Println("Migrations complete")
+	createUser(db)
+	getUsers(db)
+}
+
+func createUser(db *gorm.DB) {
+	user := User{Name: "Aknur", Age: 20}
+	db.Create(&user)
+}
+
+func getUsers(db *gorm.DB) {
+	var users []User
+	if err := db.Find(&users).Error; err != nil {
+		fmt.Println("Failed to retrieve users:", err)
+	}
+	log.Println("Retrieved Users:")
+	for _, u := range users {
+		fmt.Printf("ID: %d, Name: %s, Age: %d\n", u.ID, u.Name, u.Age)
 	}
 }
 
